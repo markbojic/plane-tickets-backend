716462e074d0d4cf0f4b9fb10d1cd92cd06ec863
Marko <marko321bojic@gmail.com>
2020-12-29 19:10:39 +0100

    Initial commit

---
+++ src/main/java/com/raf/nwp/planetickets/PlaneTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.raf.nwp.planetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raf/nwp/planetickets/PlaneTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.nwp.planetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
